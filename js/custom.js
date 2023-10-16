d4855afc93d21a05cda24e2ba6907d8c830ca931
tiantianxie <1164567053@qq.com>
2023-10-16 20:30:21 +0800

    Site updated: 2023-10-16 20:30:20

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+	if (document.hidden) {
+		$('[rel="icon"]').attr('href', "/img/newtubiao.png");
+		document.title = '天天O_O';
+		clearTimeout(titleTime);
+	} else {
+		$('[rel="icon"]').attr('href', "/img/newtubiao.png");
+		document.title = '(ฅ>ω<*ฅ) 天天开心哦~' + OriginTitle;
+		titleTime = setTimeout(function() {
+			document.title = OriginTitle;
+		}, 2000);
+	}
+});
